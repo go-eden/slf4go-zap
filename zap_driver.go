693e1577597a9c7f3af8e41d35c7772065f4e508
phenix3443 <phenix3443@gmail.com>
2020-03-04 01:44:27 +0800

    第一次实现，field没有打印，待解决

---
+++ zap_driver.go
@@ -0,0 +1,114 @@
+package slf4go_zap
+
+import (
+	"fmt"
+
+	slog "github.com/go-eden/slf4go"
+	"go.uber.org/zap"
+)
+
+type ZapDriver struct {
+	logger *zap.Logger
+	cfg    *zap.Config
+}
+
+func Init(cfg *zap.Config) {
+	d := ZapDriver{}
+	d.cfg = cfg
+	if logger, err := cfg.Build(); err != nil {
+		panic(err)
+	} else {
+		logger.Sugar().Debug("初始化成功")
+		d.logger = logger
+	}
+
+	slog.SetDriver(&d)
+}
+
+func (d *ZapDriver) Name() string {
+	return "zap"
+}
+
+func (d *ZapDriver) Print(l *slog.Log) {
+	defer d.logger.Sugar().Sync()
+
+	// if l.Fields != nil {
+	// 	fmt.Println("parse fields")
+	// 	d.cfg.InitialFields = l.Fields
+	// 	var err error
+	// 	if d.logger, err = d.cfg.Build(); err != nil {
+	// 		panic(err)
+	// 	}
+	// 	l.Fields = nil
+	// }
+	fmt.Println(l.Logger)
+	switch l.Level {
+	case slog.TraceLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Debug(l.Args...)
+		} else {
+			d.logger.Sugar().Debugf(*l.Format, l.Args...)
+		}
+	case slog.DebugLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Debug("test")
+			d.logger.Sugar().Debug(l.Args...)
+		} else {
+			d.logger.Sugar().Debugf(*l.Format, l.Args...)
+		}
+	case slog.InfoLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Info(l.Args...)
+		} else {
+			d.logger.Sugar().Infof(*l.Format, l.Args...)
+		}
+	case slog.WarnLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Warn(l.Args...)
+		} else {
+			d.logger.Sugar().Warnf(*l.Format, l.Args...)
+		}
+	case slog.ErrorLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Error(l.Args...)
+		} else {
+			d.logger.Sugar().Errorf(*l.Format, l.Args...)
+		}
+	case slog.PanicLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Panic(l.Args...)
+		} else {
+			d.logger.Sugar().Panicf(*l.Format, l.Args...)
+		}
+	case slog.FataLevel:
+		if l.Format == nil {
+			d.logger.Sugar().Fatal(l.Args...)
+		} else {
+			d.logger.Sugar().Fatalf(*l.Format, l.Args...)
+		}
+	}
+}
+
+func (d *ZapDriver) GetLevel(logger string) (sl slog.Level) {
+	l := d.cfg.Level.Level()
+
+	switch l {
+	case zap.DebugLevel:
+		sl = slog.DebugLevel
+	case zap.InfoLevel:
+		sl = slog.InfoLevel
+	case zap.WarnLevel:
+		sl = slog.WarnLevel
+	case zap.ErrorLevel:
+		sl = slog.ErrorLevel
+	case zap.DPanicLevel:
+		sl = slog.PanicLevel
+	case zap.PanicLevel:
+		sl = slog.PanicLevel
+	case zap.FatalLevel:
+		sl = slog.FataLevel
+	default:
+		sl = slog.TraceLevel
+	}
+	return
+}
---
+++ zap_driver_test.go
@@ -0,0 +1,1 @@
+package slf4go_zap
